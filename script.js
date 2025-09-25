8ec983ccc0a408f6e9bb2288d1baa7efef8505d3
Chase Lin <eoraccoofficial@gmail.com>
2025-09-24 23:43:58 -0700

    Finished

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ window.addEventListener("scroll", function () {
     header.classList.toggle("sticky", window.scrollY > 150);
 });
 
+let menu = document.querySelector('#menu-icon');
+let navlinks = document.querySelector('.navlinks');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlinks.classList.toggle('open')
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navlinks.classList.remove('open')
+}
